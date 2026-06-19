6ebf289f74a1a98f13c62ea9fb9d448d094498c6
sho <neckocr1001@gmail.com>
2026-06-19 19:54:07 +0900

    追従カメラの作成

---
+++ Assets/scripts/Camera/playerFollower.cs
@@ -0,0 +1,20 @@
+//カメラ追従
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    [SerializeField] private Transform PlayerTransform;
+    private Vector3 offset;
+    
+    void Start()
+    {
+        offset = transform.position - PlayerTransform.position;
+    }
+    void LateUpdate()
+    {
+        if (PlayerTransform != null)
+        {
+            transform.position = PlayerTransform.position + offset;
+        }
+    }
+}
